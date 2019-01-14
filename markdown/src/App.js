d5250df6cdde72c433267b818ba1119a476788bb
Alex Sellam <alex.sellam@gmail.com>
2019-01-14 17:11:04 +0100

    local storage implemented, app working

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import marked from 'marked'
+
 import { sampleText } from './sampleText'
 
 class App extends Component {
   state = {
     text: sampleText
+  }
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText})
+    }
+  }
+  componentDidUpdate () {
+    const { text } = this.state;
+    localStorage.setItem('text', text)
   }
 
   handleChange = e => {
@@ -13,6 +27,11 @@ class App extends Component {
     this.setState({
       text: text
     })
+  }
+
+  renderText = text => {
+    const __html = marked(text, {sanitize: true })
+    return { __html }
   }
   render() {
     return (
@@ -26,7 +45,7 @@ class App extends Component {
             rows="35" />
           </div>
           <div className="col-sm-6">
-          <div>{sampleText}</div>
+          <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
           </div>
         </div>
       </div>
